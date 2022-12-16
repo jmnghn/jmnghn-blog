962546365c89f0dafdfd10197e90cce0328297e7
jmnghn <jeongmyeonghyeon@gmail.com>
2022-12-16 20:07:10 +0900

    공용 Head 컴포넌트 추가

---
+++ components/common/Head/Head.tsx
@@ -0,0 +1,16 @@
+import SEO from '../SEO'
+
+const Head = () => {
+  return (
+    <SEO>
+      <meta key="charset" charSet="UTF-8" />
+      <meta
+        key="viewport"
+        name="viewport"
+        content="width=device-width, initial-scale=1"
+      />
+    </SEO>
+  )
+}
+
+export default Head
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
+import { Head } from '#components/common'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head />
+      <Component {...pageProps} />
+    </>
+  )
 }
