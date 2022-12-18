79c1ab21be341a125123054dd4d8fa78e7509a8c
jmnghn <jeongmyeonghyeon@gmail.com>
2022-12-19 04:03:12 +0900

    홈화면 폰트사이즈 반응형 적용

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <div className="bg-slate-900 w-screen h-screen flex items-center justify-center">
-      <div className="text-5xl font-semibold text-indigo-50">
+      <div className="text-4xl font-semibold text-indigo-50 sm:text-5xl">
         👷🏻: See you soon :)
       </div>
     </div>
