c9402afa556a063db822a1b7cda81d58d720515d
jmnghn <jeongmyeonghyeon@gmail.com>
2023-01-13 20:22:11 +0900

    💄 Change index page design and wording

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,17 +3,13 @@ import Layout from '#components/common/Lyaout'
 export default function Home() {
   return (
     <div>
-      <div className="bg-slate-900 w-screen h-screen flex items-center justify-center p-4 ">
-        <div
-          className="text-4xl font-semibold text-indigo-50 sm:text-5xl"
-          style={{ lineHeight: '56px' }}
-        >
+      <div className="bg-slate-900 w-screen h-screen p-4 ">
+        <div className="text-4xl font-semibold text-indigo-50 sm:text-5xl leading-tight">
           <p>
             Hello <div className="animate-waving-hand inline-block">👋🏻</div>,
           </p>
-          <p>Thank you for coming.</p>
-          <p>I&apos;m still building a blog. 🚧</p>
-          <p>👷🏻 See you shortly.</p>
+          <p>Thank you for coming. 👷🏻</p>
+          <p>I&apos;m still building a blog. 🚧 See you shortly.</p>
         </div>
       </div>
     </div>
